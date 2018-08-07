8ecc4c3cdcbeff1dc2bab4dd7449fed2aee580e2
KauyonKais <kauyonkais@outlook.de>
2018-08-08 01:24:07 +0200

    changed config.h

--- keyboards/fnh/config.h
+++ keyboards/fnh/config.h
@@ -22,6 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
+#define IGNORE_MOD_TAP_INTERRUPT
+
 #include "config_common.h"
 
 /* Use I2C or Serial, not both */
